97edd54da8ebe4914c5ba2dd099a7e2cdced3812
Diego Pupato <diego@modusense.com>
2023-05-04 15:52:07 +1200

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,44 @@ module.exports = {
         filename: "bundle.js",
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js"],
+        extensions: [".ts", ".tsx", ".js", "scss"],
         alias: {
             "@": path.join(__dirname, "src"),
         },
     },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
+            },
+        ],
+    },
     devServer: {
-        contentBase: "./public",
-        writeToDisk: true,
+        static: "./public",
         historyApiFallback: true,
+        devMiddleware: {
+            writeToDisk: true,
+        },
+        port: 3000,
     },
     externals: {
         react: "React",
